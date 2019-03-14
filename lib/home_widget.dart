3ee485482e4262662b2e14f59cdd30222671c701
ben-rieth <aurumknight3@gmail.com>
2019-03-13 19:58:32 -0500

    Implemented TabBar widget making navigation swipeable

--- lib/home_widget.dart
+++ lib/home_widget.dart
@@ -10,7 +10,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _currentIndex = 0;
   final List<Widget> _children = [
     PlaceholderWidget(Colors.white),
     PlaceholderWidget(Colors.deepOrange),
@@ -19,37 +18,39 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("LHS Connection"),
-      ),
+    return DefaultTabController(
+      length: 3,
+      child: new Scaffold(
+        appBar: AppBar(
+          title: Text("LHS Connection"),
+        ),
 
-      body: _children[_currentIndex],
+        body: TabBarView(
+          children: _children,
+        ),
 
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onTabTapped,
-        currentIndex: _currentIndex,
-        items: [
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.account_circle),
-            title: Text("Account"),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.search),
-            title: Text("Search"),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.question_answer),
-            title: Text("Something"),
-          ),
-        ],
-      ),
+        bottomNavigationBar: new TabBar(
+          tabs: [
+            Tab(
+              icon: new Icon(Icons.account_circle),
+              text: "Account",
+            ),
+            Tab(
+              icon: new Icon(Icons.search),
+              text: "Search",
+            ),
+            Tab(
+              icon: new Icon(Icons.question_answer),
+              text: "Something",
+            ),
+          ],
+          labelColor: Colors.yellow,
+          unselectedLabelColor: Colors.blue,
+          indicatorSize: TabBarIndicatorSize.tab,
+          indicatorPadding: EdgeInsets.all(5.0),
+          indicatorColor: Colors.red,
+        )
+      )
     );
-  }
-
-  void onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
   }
 }
