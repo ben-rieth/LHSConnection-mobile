6bd7a0695166f754416e75a5e75334d3fd3b9849
ben-rieth <aurumknight3@gmail.com>
2019-03-17 14:25:07 -0500

    Added: Clubs and Class list to Account page

--- lib/account_widget.dart
+++ lib/account_widget.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
 class AccountPage extends StatelessWidget {
+
+  final List<String> _extensionTileHeaders = ["Classes", "Clubs"];
+
+  final List<String> _clubList = ["Robotics", "Marching Band", "Theater"];
+  final List<String> _classList = ["Symponic Band", "AP Psychology",
+    "AP Calculus AB", "AP Literature", "AP Physics 2", "IB Computer Science 2",
+    "A+ Certification"];
+
   @override
   Widget build(BuildContext context) {
 
@@ -20,7 +28,7 @@ class AccountPage extends StatelessWidget {
             ),
 
             Padding(
-              padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+              padding: const EdgeInsets.all(8.0),
               child: Center(
                 child: Text(
                   "Name Goes Here",
@@ -32,7 +40,19 @@ class AccountPage extends StatelessWidget {
               ),
             ),
 
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: _extensionTileHeaders.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ExpansionTile(
+                    title: Text(_extensionTileHeaders[index]),
+                    children: _buildListViews(index),
+                  );
+                }
 
+              ),
+            ),
 
           ],
         ),
@@ -40,4 +60,31 @@ class AccountPage extends StatelessWidget {
     );
 
   }
+
+  List<Widget> _buildListViews(int index) {
+    switch(index) {
+      case 0: return _buildClassListView();
+      case 1: return _buildClubListView();
+      default: return <Widget>[];
+    }
+  }
+
+  List<Widget> _buildClassListView() {
+    return _classList.map<ListTile>((String className) {
+      return ListTile(
+        title: Text(className),
+      );
+    }).toList();
+  }
+
+  List<Widget> _buildClubListView() {
+    return _clubList.map<ListTile>((String clubName) {
+      return ListTile(
+        title: Text(clubName),
+      );
+    }).toList();
+  }
+
+
+
 }
