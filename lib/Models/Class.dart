cdc511da1fa01bb154ac53a6c566067ec7eebfbb
ben-rieth <aurumknight3@gmail.com>
2019-04-15 18:58:57 -0500

    Weird push

--- lib/Models/Class.dart
+++ lib/Models/Class.dart
@@ -1,24 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'package:lhs_connections/Models/Activity.dart';
+class Class  {
 
-class Class extends Activity {
+  DocumentReference reference;
+
+  String name;
+  String description;
+  IconData icon;
+  List<String> tags;
   double numCredit;
+  List<int> gradeLevels;
+  List<String> requiredCourses;
 
-  Class(String name, double numCredit, IconData icon, List<String> tags) {
-    this.name = name;
-    this.numCredit = numCredit;
-    this.icon = icon;
-    this.tags = tags;
-  }
+  Class({this.name, this.description, this.icon, this.tags,
+    this.numCredit, this.gradeLevels, this.requiredCourses});
+      //: super(name: name, description: description, icon: icon, tags: tags);
 
-  Class.description(String name, String description, double numCredit, IconData icon,
-      List<String> tags) {
-    this.name = name;
-    this.description = description;
-    this.numCredit = numCredit;
-    this.icon = icon;
-    this.tags = tags;
-  }
+  Class.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['description'] != null),
+        assert(map['icon'] != null),
+        assert(map['tags'] != null),
+        assert(map['numCredit'] != null),
+        assert(map['gradeLevels'] != null),
+        assert(map['requiredCourses'] != null),
+
+        name = map['name'],
+        description = map['description'],
+        icon = map['icon'],
+        tags = map['tags'],
+        numCredit = map['numCredit'],
+        gradeLevels = map['gradeLevels'],
+        requiredCourses = map['requiredCourses'];
+
+  Class.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
 
 }
--- lib/Models/Club.dart
+++ lib/Models/Club.dart
@@ -1,24 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'package:lhs_connections/Models/Activity.dart';
+import 'package:lhs_connections/models/User.dart';
 
-class Club extends Activity {
+class Club {
 
+  DocumentReference reference;
+
+  String name;
+  String description;
+  IconData icon;
+  List<String> tags;
   String sponsor;
 
-  Club(String name, String sponsor, IconData icon, List<String> tags) {
-    this.name = name;
-    this.sponsor = sponsor;
-    this.icon = icon;
-    this.tags = tags;
-  }
+  List<User> clubMembers;
 
-  Club.description(String name, String description, String sponsor, IconData icon,
-      List<String> tags) {
-    this.name = name;
-    this.description = description;
-    this.sponsor = sponsor;
-    this.icon = icon;
-    this.tags = tags;
-  }
+
+  Club({String name, String description, IconData icon, List<String> tags,
+    this.sponsor});
+      //: super(name: name, description: description, icon: icon, tags: tags);
+
+  Club.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['description'] != null),
+        assert(map['icon'] != null),
+        assert(map['tags'] != null),
+
+        name = map['name'],
+        description = map['description'],
+        icon = map['icon'],
+        tags = map['tags'];
+
+  Club.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
 }
