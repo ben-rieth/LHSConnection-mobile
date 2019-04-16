bf3a46f61aa4b061a77eaf5e2018326dc180b663
ben-rieth <aurumknight3@gmail.com>
2019-04-15 20:33:36 -0500

    Wrote comments for main.dart and app_state_container.dart

--- lib/app_state_container.dart
+++ lib/app_state_container.dart
@@ -9,10 +9,17 @@ import 'package:lhs_connections/models/User.dart';
 import 'package:lhs_connections/utils/grade_level.dart';
 import 'package:lhs_connections/utils/loginStatus.dart';
 
-class AppStateContainer extends StatefulWidget {
-  final AppState state;
-  final Widget child;
 
+///The AppStateContainer holds the AppState of the currently running app. This
+///   includes the currentUser, UserInformation, the status of the login, and
+///   a bool saying whether or not the app is loading.
+class AppStateContainer extends StatefulWidget {
+  final AppState state; ///the state of the currently running app
+  final Widget child; ///child of the AppStateContainer; the AppRoot
+
+  ///The constructor for AppStateContainer
+  ///   Required Params: Widget child
+  ///   Optional Params: AppState state
   AppStateContainer({
     @required this.child,
     this.state
@@ -24,22 +31,35 @@ class AppStateContainer extends StatefulWidget {
         .data;
   }
 
+  ///createState() is an inherited method from StatefulWidget which initiates
+  ///   the state of the AppStateContainer
   @override
   _AppStateContainerState createState() => new _AppStateContainerState();
 }
 
+///The state of the AppStateContainer, this holds the methods that can change
+///   the state of the app.
 class _AppStateContainerState extends State<AppStateContainer> {
-  AppState state;
+  AppState state; ///the state of the currently running app
 
-  FirebaseUser user;
+  FirebaseUser user; ///the current user
+
+  ///the authentication firebase reference
   FirebaseAuth _auth = FirebaseAuth.instance;
+
+  ///reference to the firestore database that holds user information
   CollectionReference dbUsers = Firestore.instance.collection('users');
 
   @override
   void initState() {
 
+    ///This methods checks to see if a new school year has started and therefore
+    ///   needs to update what number is associated with each grade level.
     DateUtil.checkHeaderUpdate();
 
+    ///If the state already exists, then the AppState will be set equal to it.
+    ///   Otherwise, the state will be set equal to the initial state and the
+    ///   user will be initialized.
     if(widget.state != null) {
       state = widget.state;
     } else {
@@ -51,8 +71,10 @@ class _AppStateContainerState extends State<AppStateContainer> {
     super.initState();
   }
 
+  ///initUser() is an asynchronous method that will login the user if they have
+  ///   logged it before
   Future<Null> initUser() async {
-    await _auth.signOut();
+    //await _auth.signOut();
     user = await _ensureLoggedInOnStartUp();
     if (user == null) {
       setState(() {
@@ -73,26 +95,44 @@ class _AppStateContainerState extends State<AppStateContainer> {
     return user;
   }
 
+  ///logIntoFirebase() is an asynchronous method that is called after the user
+  ///   hits the login button on the Login page. The app will attempt to find
+  ///   the user in the firebase authentication system and import the user's
+  ///   information into the app from the firestore.
+  ///
+  ///   Required Params: String email - the email the user is signing in with
+  ///                    String password - the password the user is signing in
+  ///                                      with
+  ///                    BuildContext context - the context of the login page
   Future<Null> logIntoFirebase(BuildContext context, String email, String password) async {
 
+    ///sets the state of the app to be loading
     setState(() {
       state.isLoading = true;
     });
 
-    if(user == null) {
-      //what
-    }
-
     try {
+      ///Attempts to sign the user in with the given email and password, this
+      ///   might throw a USER_NOT_FOUND error or WRONG_PASSWORD error
       user = await _auth.signInWithEmailAndPassword(
           email: email, password: password);
 
 
+      ///If the user is signed in, the app will gather the user's information
+      ///   from the firestore or create the information if there is no
+      ///   information associated with the student
       if(user != null) {
+        ///result is the document in the 'users' firestore that has an identical
+        ///   id to the signed in firebase user
         final QuerySnapshot result = await dbUsers.where('id', isEqualTo: user.uid).getDocuments();
+
+        ///documents is the list of documents inside of the user document
         final List<DocumentSnapshot> documents = result.documents;
 
-
+        ///if there are no documents in the user's document, meaning this is the
+        ///   user's first time signing into the app and therefore has no
+        ///   information in their account yet, the app will create the
+        ///   infromation
         if(documents.length == 0) {
           dbUsers
               .document(user.uid)
@@ -104,10 +144,11 @@ class _AppStateContainerState extends State<AppStateContainer> {
           });
         }
 
+        ///creates a user object with the user's information inside the app
         User userInformation = await _createUserInformation();
 
-        print("USERINFO: $userInformation");
-
+        ///sets the user, user information, and a successful login status to the
+        ///   app's state. Also tells the app to stop loading.
         setState(() {
           state.isLoading = false;
           state.currentUser = user;
@@ -118,11 +159,18 @@ class _AppStateContainerState extends State<AppStateContainer> {
       }
     } catch (e) {
 
+      ///This error is thrown if the user is not found in the firebase, meaning
+      ///   the email is incorrect. Removes the app from loading and sets the
+      ///   login status to UserNotFound.
       if (e.toString().contains("ERROR_USER_NOT_FOUND")) {
         setState(() {
           state.isLoading = false;
           state.loginStatus = LoginStatus.UserNotFound;
         });
+
+      ///This error is thrown if the email is correct but the password is not.
+      ///   Removes the app from loading and sets the login status to
+      ///   PasswordIncorrect
       } else if (e.toString().contains("ERROR_WRONG_PASSWORD")) {
         setState(() {
           state.isLoading = false;
@@ -134,8 +182,13 @@ class _AppStateContainerState extends State<AppStateContainer> {
     }
   }
 
+  /// _createUserInformation() is a private async method that retrieves all of
+  ///   the user's information form the firestore and loads it into the app
   Future<User> _createUserInformation() async {
     User userInfo;
+
+    ///gets the documents associated with the id of the firebase user and builds
+    ///   a user object out of them.`
     userInfo = await dbUsers.document(user.uid).get().then((DocumentSnapshot ds) {
       return User.fromSnapshot(ds);
     });
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:lhs_connections/app_root.dart';
 import 'package:lhs_connections/app_state_container.dart';
 
-
+/// The main() function is the entrypoint to the app.
+/// The runApp() method is run by main() and it creates the Widget tree for the
+///   app, starting the tree with an AppStateContainer.
+/// The AppStateContainer holds the state of the app and has a child widget of
+///   AppRoot, which begins to build the UI of the app.
 void main() {
   runApp(new AppStateContainer(
     child: new AppRoot(),
