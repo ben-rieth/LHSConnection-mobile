00cc85710c57f32ae5d74233c2b63fbff553329c
ben-rieth <aurumknight3@gmail.com>
2019-05-12 16:49:36 -0500

    Removed: Unused imports from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:lhs_connections/app_root.dart';
-import 'package:lhs_connections/blocs/authentication_bloc/bloc.dart';
-import 'package:lhs_connections/repositories/user_repository.dart';
-import 'package:lhs_connections/widgets/home_screen.dart';
-import 'package:lhs_connections/widgets/login_widgets/splash_screen.dart';
 import 'package:lhs_connections/blocs/simple_bloc_delegate.dart';
-import 'package:lhs_connections/widgets/bottom_nav_bar/account_screen.dart';
-
-import 'package:lhs_connections/widgets/login_widgets/login_screen.dart';
-import 'package:lhs_connections/widgets/login_widgets/login_form.dart';
-import 'package:lhs_connections/blocs/login_bloc/bloc.dart';
-import 'package:lhs_connections/widgets/login_widgets/login_button.dart';
 
 main() {
   BlocSupervisor().delegate = SimpleBlocDelegate();
