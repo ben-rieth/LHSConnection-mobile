e6262e803c7091e2627a3991215686eb459f97d7
ben-rieth <aurumknight3@gmail.com>
2019-05-12 16:32:38 -0500

    Edited splash screen

--- lib/widgets/login_widgets/splash_screen.dart
+++ lib/widgets/login_widgets/splash_screen.dart
@@ -4,7 +4,28 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Splash Screen')),
+      body:Align(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+
+            CircleAvatar(
+              backgroundColor: Colors.transparent,
+              radius: 100.0,
+              child: Image.asset('assets/lhsConnections.png',),
+            ),
+
+            SizedBox(height: 30.0),
+
+            CircularProgressIndicator(
+              backgroundColor: Theme.of(context).primaryColor,
+            ),
+
+         ],
+        )
+      ),
     );
   }
 }
