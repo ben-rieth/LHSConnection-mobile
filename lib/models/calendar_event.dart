6dcacdc64e6bbf1560b566d2a2fa9b8e3cd6d856
benrieth <benrieth3@gmail.com>
2019-07-02 16:23:22 -0500

    Added calendar event model class

---
+++ lib/models/calendar_event.dart
@@ -0,0 +1,13 @@
+class CalendarEvent {
+
+  String eventName;
+  DateTime startTime;
+  DateTime endTime;
+  DateTime day;
+
+  CalendarEvent({this.eventName, this.startTime, this.endTime, this.day});
+
+
+
+
+}
